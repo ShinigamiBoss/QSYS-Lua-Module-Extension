c965143cbc46a676770db25b695dd908f84fba0a
ShinigamiBoss <fabio.feliciosi@hotmail.it>
2021-05-14 15:23:16 +0200

    Added Tcp Socket Server

--- QSYS Lua Extension.lua
+++ QSYS Lua Extension.lua
@@ -885,3 +885,18 @@ function TcpSocket:ReadLine(EOL, custom) end
 --start_pos : optional index where to start to search, default value is 1
 function TcpSocket:Search(str, start_pos) end
 
+--The TcpServer object allows Q-SYS cores to accept client TCP/IP connections from devices on the network. 
+TcpSocketServer = {}
+
+--Creates a new TcpServer instance
+function TcpSocketServer:New() end
+
+--Open a port and listen for incoming connection
+--port : port number for listen to incoming connection
+function TcpSocketServer:Listen(port) end
+
+--Close the listeing port and disconnect all the connected clients
+function TcpSocketServer:Close() end
+
+--Function called on any incoming socket event. 
+function TcpSocketServer.EventHandler() end
